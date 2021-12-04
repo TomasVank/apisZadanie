91d73136b6882c93a4ec6b6e813995bd5eb0029c
TomasVank <72217029+TomasVank@users.noreply.github.com>
2021-12-04 16:07:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -58,5 +58,52 @@ def GetCustomers():
   myDb.close()
   return jsonify(result),200
 
+@app.route('/CreateOrder', methods=["POST"])
+def CreateOrder():
+  data = request.get_json(force=True)
+  order_dict = dict(data)
+  with open ('content/OrderInsertTable.ddl') as ddl_file:
+    sql = ddl_file.read()
+    sql = sql[:-1] + "'{}','{}','{}','{}')"
+  myDb = MYSQL.connect(host="147.232.40.14", user="tv635vg", passwd="Airi8Eiw", database="tv635vg")
+  cursor = myDb.cursor()
+  cursor.execute(sql.format(order_dict["orders_ID"], order_dict["product_ID"],order_dict["customer_ID"],order_dict["orders_quantity"]))
+  myDb.commit()
+  cursor.close()
+  myDb.close()
+  return jsonify("Created"),201
+
+
+@app.route('/CreateCustomer', methods=["POST"])
+def CreateCustomer():
+  data = request.get_json(force=True)
+  customer_dict = dict(data)
+  with open ('content/CustomerInsertTable.ddl') as ddl_file:
+    sql = ddl_file.read()
+    sql = sql[:-1] + "'{}','{}','{}','{}','{}','{}')"
+  myDb = MYSQL.connect(host="147.232.40.14", user="tv635vg", passwd="Airi8Eiw", database="tv635vg")
+  cursor = myDb.cursor()
+  cursor.execute(sql.format(customer_dict["customer_ID"], customer_dict["customer_firstName"],customer_dict["customer_lastName"],customer_dict["customer_phone"],customer_dict["customer_email"],customer_dict["customer_address"]))
+  myDb.commit()
+  cursor.close()
+  myDb.close()
+  return jsonify("Created"),201
+
+@app.route('/CreateProduct', methods=["POST"])
+def CreateProduct():
+  data = request.get_json(force=True)
+  product_dict = dict(data)
+  with open ('content/ProductInsertTable.ddl') as ddl_file:
+    sql = ddl_file.read()
+    sql = sql[:-1] + "'{}','{}','{}','{}')"
+  myDb = MYSQL.connect(host="147.232.40.14", user="tv635vg", passwd="Airi8Eiw", database="tv635vg")
+  cursor = myDb.cursor()
+  cursor.execute(sql.format(product_dict["product_ID"], product_dict["product_name"], product_dict["product_price"], product_dict["product_pieces_WH"]))
+  myDb.commit()
+  cursor.close()
+  myDb.close()
+  return jsonify("Created"),201
+
+
 if __name__ == "__main__":
     app.run()
