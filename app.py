bf40f260a95716a95f0a17ccb925f4d1a37777fd
TomasVank <72217029+TomasVank@users.noreply.github.com>
2021-12-04 16:20:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -104,6 +104,47 @@ def CreateProduct():
   myDb.close()
   return jsonify("Created"),201
 
+@app.route('/UpdateOrder/<id>', methods=['PUT'])
+def UpdateOrder(id):
+  data = request.get_json(force=True)
+  order_dict = dict(data)
+  with open ('content/OrderUpdateTable.ddl') as ddl_file:
+    sql = ddl_file.read()
+  myDb = MYSQL.connect(host="147.232.40.14", user="tv635vg", passwd="Airi8Eiw", database="tv635vg")
+  cursor = myDb.cursor()
+  cursor.execute(sql.format(order_dict["orders_ID"], order_dict["product_ID"],order_dict["customer_ID"],order_dict["orders_quantity"],order_dict["orders_ID"]))
+  myDb.commit()
+  cursor.close()
+  myDb.close()
+  return jsonify("Updated"),200
+
+@app.route('/UpdateCustomer/<id>', methods=['PUT'])
+def UpdateCustomer(id):
+  data = request.get_json(force=True)
+  customer_dict = dict(data)
+  with open ('content/CustomerUpdateTable.ddl') as ddl_file:
+    sql = ddl_file.read()
+  myDb = MYSQL.connect(host="147.232.40.14", user="tv635vg", passwd="Airi8Eiw", database="tv635vg")
+  cursor = myDb.cursor()
+  cursor.execute(sql.format(customer_dict["customer_ID"], customer_dict["customer_firstName"],customer_dict["customer_lastName"],customer_dict["customer_phone"],customer_dict["customer_email"],customer_dict["customer_address"],customer_dict["customer_ID"]))
+  myDb.commit()
+  cursor.close()
+  myDb.close()
+  return jsonify("Updated"),200
+
+@app.route('/UpdateProduct/<id>', methods=['PUT'])
+def UpdateProduct(id):
+  data = request.get_json(force=True)
+  product_dict = dict(data)
+  with open ('content/ProductUpdateTable.ddl') as ddl_file:
+    sql = ddl_file.read()
+  myDb = MYSQL.connect(host="147.232.40.14", user="tv635vg", passwd="Airi8Eiw", database="tv635vg")
+  cursor = myDb.cursor()
+  cursor.execute(sql.format(product_dict["product_ID"], product_dict["product_name"],product_dict["product_price"],product_dict["product_pieces_WH"],product_dict["product_ID"]))
+  myDb.commit()
+  cursor.close()
+  myDb.close()
+  return jsonify("Updated"),200
 
 if __name__ == "__main__":
     app.run()
